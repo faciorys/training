8928bd8c1d65d3148c7cbd78c23a601835cc5c32
faciorys <85235128+faciorys@users.noreply.github.com>
2021-06-30 15:20:48 +0300

    Update Main.java

--- Task4/src/epam/task/task07/Main.java
+++ Task4/src/epam/task/task07/Main.java
@@ -2,6 +2,10 @@ package epam.task.task07;
 
 import java.util.Scanner;
 
+/*
+* 7. Описать класс, представляющий треугольник. Предусмотреть методы для создания объектов, вычисления
+* площади, периметра и точки пересечения медиан.
+* */
 public class Main {
     public static void main(String[] args) {
         Triangle one = new Triangle(3,4);
