e12c8d123d3aba6b9f440355849c9d848317c806
faciorys <85235128+faciorys@users.noreply.github.com>
2021-06-30 15:15:27 +0300

    Update Main.java

--- Task4/src/epam/task/task12/Main.java
+++ Task4/src/epam/task/task12/Main.java
@@ -2,6 +2,8 @@ package epam.task.task12;
 
 import java.util.Scanner;
 
+// Создать объект класса Автомобиль, используя классы Колесо, Двигатель. Методы: ехать, заправляться,
+//менять колесо, вывести на консоль марку автомобиля.
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
