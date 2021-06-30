90fd98509794af9f9c7a37bd94038bec09b417b7
faciorys <85235128+faciorys@users.noreply.github.com>
2021-06-30 15:14:32 +0300

    Update Main.java

--- Task4/src/epam/task/task13/Main.java
+++ Task4/src/epam/task/task13/Main.java
@@ -1,6 +1,7 @@
 package epam.task.task13;
 
-
+// Создать объект класса Государство, используя классы Область, Район, Город. Методы: вывести на консоль
+//столицу, количество областей, площадь, областные центры.
 public class Main {
     public static void main(String[] args) {
         Country country = new Country("Беларусь", "Минск");
